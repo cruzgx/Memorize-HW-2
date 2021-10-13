1a900f0b57384afcadd0dcec20a545c26ad54132
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-10-13 00:05:08 -0400

    Added colors to Memorize Game

--- HW 2/ContentView.swift
+++ HW 2/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
                             .onTapGesture {
                                 viewModel.choose(card)
                             }
-                            
+                            .foregroundColor(viewModel.getThemeColor())
                     }
                 }
 
